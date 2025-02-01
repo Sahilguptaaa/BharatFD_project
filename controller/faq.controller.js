b89e57499335530e2a89110b449872442bb98d07
sahil gupta <sahilgupta16308@gmail.com>
2025-02-01 17:08:33 +0530

    added faq.controller.js file in controller folder

---
+++ controller/faq.controller.js
@@ -0,0 +1,25 @@
+import FAQ from "../models/faq.model";
+import translateText from "../utils/translate.js";
+
+export const fetchFaqController = async (req, res) => {};
+
+export const createFaqController = async (req, res) => {
+  const { question, answer } = req.body;
+  try {
+    const translatedQuestions = await translateText(question);
+    const translatedAnswers = await translateText(answer);
+
+    const translations = {};
+    Object.keys(translatedQuestions).forEach((lang) => {
+      translations[lang] = {
+        question: translatedQuestions[lang],
+        answer: translatedAnswers[lang],
+      };
+    });
+    
+    const newFaq = await FAQ.create({ question, answer, translations });
+    res.status(201).json(newFaq);
+  } catch (error) {
+    res.status(500).json({ message: "Error saving FAQ", error });
+  }
+};
