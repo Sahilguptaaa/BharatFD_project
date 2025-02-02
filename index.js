c70f3fcc07758ea6b34ea6401a4d00e30e714599
sahil gupta <sahilgupta16308@gmail.com>
2025-02-02 18:53:42 +0530

    add test

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import "dotenv/config";
 import express from "express";
-import connectDB from "./db/conn.js";
+import connectDB from "./db/index.js";
 
 connectDB();
 const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-import api from "./routes/api.routes.js";
+import api from "./routes/api.route.js";
 app.use("/api", api);
 
 app.get("/", (req, res) => {
-  res.json({ hello: "sam" });
+  res.json({ message : "welcome to bharatfd" });
 });
 
 const port = process.env.PORT || 3000;
